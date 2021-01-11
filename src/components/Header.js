0f4f48f796323571c86cf0b8fe83571f4f2acbfe
scott-olson <scotteolson3@gmail.com>
2021-01-11 11:59:31 -0800

    add background color swatches to colorPalette.css and test implementation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
+import { Container, Row, Col, Navbar } from 'react-bootstrap';
+import '../colorPalette.css';
 
 export default function Header() {
     return (
-        <Container></Container>
+        <Navbar className="bg-secondary-1-2" variant="dark">
+            <Navbar.Brand href="#home">
+                <img
+                    alt=""
+                    src="../favicon.ico"
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                />{' '}
+            Brol Research
+            </Navbar.Brand>
+        </Navbar>
     )
 }
